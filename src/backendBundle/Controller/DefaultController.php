7863ba6d4fd68a541ffaf34000c35626b5f14830
gchiappe <gchiappe@magyp.gob.ar>
2019-01-07 15:57:42 -0300

    configuracion manual de YML

--- src/backendBundle/Controller/DefaultController.php
+++ src/backendBundle/Controller/DefaultController.php
@@ -8,6 +8,18 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+
+    	$em= $this->getDoctrine()->getManager();
+    	$user_repo =$em->getRepository('backendBundle:User');
+
+    	$user = $user_repo->find(1);
+    	//$user = $user_repo->findAll();
+
+    
+    	//var_dump($user);
+    	echo "Bienvenido ".$user->getName()." ".$user->getSurname();
+    	die();
+
         return $this->render('backendBundle:Default:index.html.twig');
     }
 }
