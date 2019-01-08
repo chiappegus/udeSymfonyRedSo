b4b7ab3c302b9506490c9aa80d0fe882ad4d188b
gchiappe <gchiappe@magyp.gob.ar>
2019-01-08 15:14:11 -0300

    views and layauts

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
     public function loginAction(Request $request)
     {
          echo "accion login";
-            die();
+            //die();
 
      	return $this->render('AppBundle:User:login.html.twig',
      		array("titulo"=>"Login" ));
